422bdfbd0c75d11121dedfc9387f22694f72e7aa
BKarpow <xymerone@gmail.com>
2022-10-27 09:29:25 +0300

    Fix bugs, access control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pyaimp
 import telebot
-import time
 import query_lib as q
 from os import getenv
 from telebot import types
@@ -9,15 +8,17 @@ from pathlib import Path
 
 log_file = Path('accsss.log')
 logger.add(str(log_file.absolute()), rotation="20 KB", compression="zip")
-
-# @rs1221_bot bot name
+# Access control from chat id
+verify_chat_ids = getenv("ACCESS_CHAT_IDS").split(',')
 bot = telebot.TeleBot(getenv("TG_TOKEN"))
 
+
 def client_aimp():
-    try:
-        return pyaimp.Client()
-    except RuntimeError as e:
-        logger.error(e)
+    return pyaimp.Client()
+
+
+def is_verify_user(message) -> bool:
+    return str(message.chat.id) in verify_chat_ids
 
 
 def get_keyboard():
@@ -40,55 +41,88 @@ def get_keyboard():
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
     logger.info(f"Run command start. Chat id: {message.chat.id}")
-    aimp = client_aimp()
-    info = aimp.get_current_track_info()
-    msg = f"Зараз грає\n{info['artist']} {info['title']}."
-    bot.send_message(message.chat.id, msg, reply_markup=get_keyboard())
+    if not is_verify_user(message):
+        logger.error(f"Немає у Вас доступу, шановний. Chat id: {message.chat.id}")
+        bot.send_message(message.chat.id, "Немає у Вас доступу, шановний!")
+        return
+    try:
+        aimp = client_aimp()
+        info = aimp.get_current_track_info()
+        msg = f"Зараз грає\n{info['artist']} {info['title']}."
+        bot.send_message(message.chat.id, msg, reply_markup=get_keyboard())
+    except RuntimeError as e:
+        logger.error(f"AIMP not running: {e}")
+        bot.send_message(
+            message.chat.id,
+            "AIMP не запушено! Запустіть AIMP та виконаййте крманду /start ."
+        )
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-    amp = client_aimp()
-    if call.message:
-        if call.data == 'prev':
-            q.go_prev(bot, amp, call, get_keyboard)
-        elif call.data == 'update':
-            q.go_update(bot, amp, call, get_keyboard)
-        elif call.data == 'next':
-            q.go_next(bot, amp, call, get_keyboard)
-        elif call.data == 'play_pause':
-            q.go_play_pause(bot, amp, call, get_keyboard)
-        elif call.data == "shuffle":
-            q.go_shuffle(bot, amp, call, get_keyboard)
-        elif call.data == "vol_25":
-            q.go_set_volume(25, bot, amp, call, get_keyboard)
-        elif call.data == "vol_50":
-            q.go_set_volume(50, bot, amp, call, get_keyboard)
-        elif call.data == "vol_75":
-            q.go_set_volume(75, bot, amp, call, get_keyboard)
-        elif call.data == "vol_100":
-            q.go_set_volume(100, bot, amp, call, get_keyboard)
-        else:
-            q.go_no_command(bot, call, get_keyboard)
+    try:
+        amp = client_aimp()
+        if call.message:
+            if call.data == 'prev':
+                q.go_prev(bot, amp, call, get_keyboard)
+            elif call.data == 'update':
+                q.go_update(bot, amp, call, get_keyboard)
+            elif call.data == 'next':
+                q.go_next(bot, amp, call, get_keyboard)
+            elif call.data == 'play_pause':
+                q.go_play_pause(bot, amp, call, get_keyboard)
+            elif call.data == "shuffle":
+                q.go_shuffle(bot, amp, call, get_keyboard)
+            elif call.data == "vol_25":
+                q.go_set_volume(25, bot, amp, call, get_keyboard)
+            elif call.data == "vol_50":
+                q.go_set_volume(50, bot, amp, call, get_keyboard)
+            elif call.data == "vol_75":
+                q.go_set_volume(75, bot, amp, call, get_keyboard)
+            elif call.data == "vol_100":
+                q.go_set_volume(100, bot, amp, call, get_keyboard)
+            else:
+                q.go_no_command(bot, call, get_keyboard)
+    except RuntimeError as e:
+        logger.error(f"AIMP not running: {e}")
+        bot.send_message(
+            call.message.chat.id,
+            "AIMP не запушено! Запустіть AIMP та виконаййте крманду /start ."
+        )
 
 
 @bot.message_handler(commands=['mute'])
 def com_mute(message):
-    c = client_aimp()
-    c.set_muted(not(c.is_muted()))
-    bot.send_message(message.chat.id, "Мут ...")
+    try:
+        c = client_aimp()
+        c.set_muted(not(c.is_muted()))
+        bot.send_message(message.chat.id, "Мут ...")
+    except RuntimeError as e:
+        logger.error(f"AIMP not running: {e}")
+        bot.send_message(
+            message.chat.id,
+            "AIMP не запушено! Запустіть AIMP та виконаййте крманду /start ."
+        )
 
 
 @bot.message_handler(commands=['info'])
 def com_info(message):
-    c = client_aimp()
-    info = c.get_current_track_info()
-    msg = f"Зараз грає\n{info['artist']} {info['title']}."
-    bot.send_message(message.chat.id, msg)
+    try:
+        c = client_aimp()
+        info = c.get_current_track_info()
+        msg = f"Зараз грає\n{info['artist']} {info['title']}."
+        bot.send_message(message.chat.id, msg)
+    except RuntimeError as e:
+        logger.error(f"AIMP not running: {e}")
+        bot.send_message(
+            message.chat.id,
+            "AIMP не запушено! Запустіть AIMP та виконаййте крманду /start ."
+        )
 
 
 if __name__ == '__main__':
     logger.info("Bot running!")
+    logger.debug(f"Verify access from chat ids {verify_chat_ids}")
     try:
         bot.infinity_polling()
     except:
